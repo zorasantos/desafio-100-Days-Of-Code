a25bef87c294a1f963203630611497d45141ee6a
Zoranildo <zoranildo22@gmail.com>
2019-07-21 22:56:34 -0300

    Tenth Day

--- Tenth Day/pizzaria/src/App.js
+++ Tenth Day/pizzaria/src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { MainPage } from './pages/main'
+import { Login } from './pages/login/'
 
-export default App;
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={MainPage} exact />
+      <Route path='/login' component={Login} />
+    </Switch>
+  </BrowserRouter>
+)
+export default App
