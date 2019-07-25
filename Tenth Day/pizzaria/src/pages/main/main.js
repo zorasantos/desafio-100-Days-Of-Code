6799116abedd9f5bb9b45f78635696cca8119073
Zoranildo <zoranildo22@gmail.com>
2019-07-25 12:55:03 -0300

    fourteenth day

--- Tenth Day/pizzaria/src/pages/main/main.js
+++ Tenth Day/pizzaria/src/pages/main/main.js
@@ -1,43 +1,61 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import styled from 'styled-components'
 import {
   AppBar,
-  Toolbar,
+  Toolbar as MaterialToolbar,
   IconButton,
   Typography,
   Menu,
   MenuItem } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { ReactComponent as MainLogo } from '../../pages/login/logo.svg'
+import { AuthContext } from '../../contexts/auth'
 
-const MainPage = () => (
-  <AppBar>
-    <Toolbar>
-      <LogoContainer>
-        <Logo />
-      </LogoContainer>
+const MainPage = () => {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const { userInfo, logout } = useContext(AuthContext)
 
-      <Typography color='inherit'>
-        Ola João =)
-      </Typography>
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
 
-      <IconButton color='inherit'>
-        <AccountCircle />
-      </IconButton>
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
 
-      <Menu open={false}>
-        <MenuItem>
-          Sair
-        </MenuItem>
-      </Menu>
+  return (
+    <AppBar>
+      <Toolbar>
+        <LogoContainer>
+          <Logo />
+        </LogoContainer>
 
-    </Toolbar>
-  </AppBar>
-)
+        <Typography color='inherit'>
+          Ola {userInfo.user.displayName.split(' ')[0]} =)
+        </Typography>
 
+        <IconButton color='inherit' onClick={handleOpenMenu}>
+          <AccountCircle />
+        </IconButton>
+
+        <Menu open={!!anchorElement} onClose={handleClose} anchorEl={anchorElement}>
+          <MenuItem onClick={logout}>
+            Sair
+          </MenuItem>
+        </Menu>
+
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+const Toolbar = styled(MaterialToolbar)`
+  margin: 0 auto;
+  max-width: 960px;
+  width: 100%;
+`
 const LogoContainer = styled.div`
   flex-grow: 1
-
 `
 
 const Logo = styled(MainLogo)`
@@ -51,7 +69,6 @@ const Logo = styled(MainLogo)`
   & line {
     stroke: #fff;
   }
-
 `
 
 export default MainPage
