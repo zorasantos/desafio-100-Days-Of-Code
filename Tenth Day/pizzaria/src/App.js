15a8c046054ff7df72f0bdc5e7ce395dafc4685d
Zoranildo <zoranildo22@gmail.com>
2019-07-25 02:55:00 -0300

    fourteenth day

--- Tenth Day/pizzaria/src/App.js
+++ Tenth Day/pizzaria/src/App.js
@@ -1,5 +1,6 @@
-import React, { lazy, useContext, useEffect, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { lazy, useContext, useEffect, useState, Suspense } from 'react'
+import t from 'prop-types'
+import { Redirect, Route, Switch } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
 import firebase from './services/firebase'
 import { AuthContext } from './contexts/auth'
@@ -11,8 +12,11 @@ import { AuthContext } from './contexts/auth'
 const MainPage = lazy(() => import('./pages/main'))
 const Login = lazy(() => import('./pages/login'))
 
-function App () {
-  const { setUserInfo } = useContext(AuthContext)
+function App ({ location }) {
+  const { userInfo, setUserInfo, logout } = useContext(AuthContext)
+  const [didCheckUserIn, setDidCheckUserIn] = useState(false)
+
+  const { isUserLoggedIn } = userInfo
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged((user) => {
@@ -21,8 +25,23 @@ function App () {
         isUserLoggedIn: !!user,
         user
       })
+      setDidCheckUserIn(true)
     })
-  }, [setUserInfo])
+
+    window.logout = logout
+  }, [setUserInfo, logout, setDidCheckUserIn])
+
+  if (!didCheckUserIn) {
+    return <LinearProgress />
+  }
+
+  if (isUserLoggedIn && location.pathname === '/login') {
+    return <Redirect to='/' />
+  }
+
+  if (!isUserLoggedIn && location.pathname !== '/login') {
+    return <Redirect to='/login' />
+  }
 
   return (
     <Suspense fallback={<LinearProgress />}>
@@ -32,5 +51,9 @@ function App () {
       </Switch>
     </Suspense>
   )
+}
+
+App.propTypes = {
+  location: t.object.isRequired
 }
 export default App
